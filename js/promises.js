19d686cc5d206f10f2b7a1efde4244b2bf10ee3c
Jean-Baptiste <jbvilain@gmail.com>
2018-04-13 13:58:18 +0200

    Update promises.js

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,5 @@
-var delayedCall = function(delayMs, callLabel) { return new Promise(function(resolve, reject) {setTimeout(function() {console.log('test'); resolve();}, delayMs)}); }
+var delayedCall = function(delayMs, callLabel) { return new Promise(function(resolve, reject) {setTimeout(function() {console.log(callLabel); resolve();}, delayMs)}); }
 
-var appelDistant1 = function() {return delayedCall(1000, 'appelDistant1');}
-var appelLocal2 = function() {return delayedCall(0, 'appelLocal2');}
-var appelDistant3 = function() {return delayedCall(500, 'appelDistant3');}
+var appelDistant1 = function() {return delayedCall(1000, 'appelDistant1 - Réponse après 1000ms');}
+var appelLocal2 = function() {return delayedCall(0, 'appelLocal2 - Réponse instantanée');}
+var appelDistant3 = function() {return delayedCall(500, 'appelDistant3 - Réponse après 500ms');}
